dbc28c83ad6db98e2fe1351d5b4f09f0d8002020
Sagar Yadav <sagaryadav478@gmail.com>
2023-11-05 11:48:40 +0530

    Completed catalog feature

---
+++ src/catalog/Book.java
@@ -0,0 +1,36 @@
+package catalog;
+
+import java.util.Date;
+
+public class Book {
+    String name;
+
+    String author;
+
+    Date publishedDate;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+}
